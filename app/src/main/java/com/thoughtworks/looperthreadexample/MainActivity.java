16fba7d0c8967cfd6e20d6ee00202835ba4d776a
zdazx <zhangduo0227@163.com>
2020-07-23 22:12:53 +0800

    implement number's changing in thread

--- app/src/main/java/com/thoughtworks/looperthreadexample/MainActivity.java
+++ app/src/main/java/com/thoughtworks/looperthreadexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.thoughtworks.looperthreadexample;
 
 import android.os.Bundle;
 import android.os.Message;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,7 @@ import static com.thoughtworks.looperthreadexample.MyHandler.TASK_B;
 
 public class MainActivity extends AppCompatActivity {
     private LooperThread looperThread;
+    private Button threadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button aBtn = findViewById(R.id.a_btn);
         Button bBtn = findViewById(R.id.b_btn);
+        threadBtn = findViewById(R.id.thread_btn);
 
         aBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +40,28 @@ public class MainActivity extends AppCompatActivity {
                 triggerToast(TASK_B);
             }
         });
+
+        threadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeNumber();
+            }
+        });
+    }
+
+    private void changeNumber() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                threadBtn.setClickable(false);
+                for (int i = 1; i <= 10; i++) {
+                    threadBtn.setText(String.valueOf(i));
+                    SystemClock.sleep(1000);
+                }
+                threadBtn.setText(R.string.thread_btn);
+                threadBtn.setClickable(true);
+            }
+        }).start();
     }
 
     private void triggerToast(int what) {
